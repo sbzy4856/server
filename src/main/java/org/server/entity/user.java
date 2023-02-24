bcd8b665aa7c2535db8a7ff7030bba559df0c7f2
李波 <libo@jiastudy.com>
2023-02-24 21:46:47 +0800

    用户

---
+++ src/main/java/org/server/entity/user.java
@@ -0,0 +1,42 @@
+package org.server.entity;
+
+public class user {
+    private int userId;
+    private String username;
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    private String userType;
+
+}
