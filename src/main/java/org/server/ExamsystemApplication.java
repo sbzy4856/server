cf2bf6123608573e8c4ec9eee5829c743a4200c9
李波 <libo@jiastudy.com>
2023-02-24 14:45:16 +0800

    1

---
+++ src/main/java/org/server/ExamsystemApplication.java
@@ -0,0 +1,14 @@
+package org.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ExamsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsystemApplication.class, args);
+    }
+
+}
+
